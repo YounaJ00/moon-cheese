fb6d9782b87589175441ee02cc9a53f6d7fa5619
Youna Joo <you.na.joo0@gmail.com>
2025-11-28 11:28:35 +0900

    feat: 환율 타입 추가

---
+++ src/types/currency.ts
@@ -0,0 +1,10 @@
+export type CurrencyType = 'USD' | 'KRW';
+
+export type ExchangeRate = {
+  USD: number;
+  KRW: number;
+};
+
+export type ExchangeRateResponse = {
+  exchangeRate: ExchangeRate;
+};

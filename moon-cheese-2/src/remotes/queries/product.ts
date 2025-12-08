8316eb7c076db6fc6651126ec003b0b33997c709
Youna Joo <you.na.joo0@gmail.com>
2025-12-08 19:10:53 +0900

    feat: 상품 목록 쿼리 옵션 추가

--- moon-cheese-2/src/remotes/queries/product.ts
+++ moon-cheese-2/src/remotes/queries/product.ts
@@ -1,8 +1,15 @@
 import { queryOptions } from '@tanstack/react-query';
-import { getRecentProductList } from '../product';
+import { getProductList, getRecentProductList } from '../product';
 
 export const getRecentProductListQueryOptions = () =>
   queryOptions({
     queryKey: ['recent-product-list'],
     queryFn: getRecentProductList,
   });
+
+export const getProductListQueryOptions = () => {
+  return queryOptions({
+    queryKey: ['product-list'],
+    queryFn: getProductList,
+  });
+};

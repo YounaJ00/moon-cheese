604351e1a66acdd818b464c3a896b8b0c1f51f89
Youna Joo <you.na.joo0@gmail.com>
2025-12-08 19:46:25 +0900

    feat: 환율 인자 추가

--- moon-cheese-2/src/format/number.ts
+++ moon-cheese-2/src/format/number.ts
@@ -1,11 +1,18 @@
 import type { Currency } from '@/type';
 
-export const formatPriceByCurrency = (price: number, currency: Currency) => {
+export const formatPriceByCurrency = (price: number, currency: Currency, rate: Record<Currency, number>) => {
+  const value = currency === 'USD' ? price : Math.round(price * rate.KRW);
   switch (currency) {
     case 'KRW':
-      return `${Math.round(price).toLocaleString('ko-KR')}원`;
-    case 'USD':
-      return `$${price.toLocaleString('en-US')}`;
+      return `${value.toLocaleString('ko-KR')}원`;
+    case 'USD': {
+      const hasDecimal = value % 1 !== 0;
+      const decimal = hasDecimal ? 2 : 0;
+      return `$${value.toLocaleString('en-US', {
+        minimumFractionDigits: decimal,
+        maximumFractionDigits: decimal,
+      })}`;
+    }
     default:
       currency satisfies never;
       return;

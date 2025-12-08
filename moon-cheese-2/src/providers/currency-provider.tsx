5f640c1ae13325dcc9747dab3d0f80d13de84d0c
Youna Joo <you.na.joo0@gmail.com>
2025-12-08 20:54:33 +0900

    feat: 통화 컨텍스트에 서버 환율 데이터 연동

--- moon-cheese-2/src/providers/currency-provider.tsx
+++ moon-cheese-2/src/providers/currency-provider.tsx
@@ -1,15 +1,21 @@
-import { useSuspenseQuery } from '@tanstack/react-query';
 import { createContext, useContext, useState } from 'react';
-import { getExchangeRateQueryOptions } from '@/remotes/queries/exchange';
 import type { Currency } from '@/type';
+import { getExchangeRateQueryOptions } from '@/remotes/queries/exchange';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import type { ExchangeRate } from '@/remotes/exchange';
 
 type CurrencyContextType = {
   currency: Currency;
+  exchangeRate: ExchangeRate;
   setCurrency: (currency: Currency) => void;
 };
 
-const currencyContext = createContext<Pick<CurrencyContextType, 'currency'>>({
+const currencyContext = createContext<Pick<CurrencyContextType, 'currency' | 'exchangeRate'>>({
   currency: 'USD',
+  exchangeRate: {
+    KRW: 1,
+    USD: 1,
+  },
 });
 
 const currencyActionContext = createContext<Pick<CurrencyContextType, 'setCurrency'>>({
@@ -17,9 +23,10 @@ const currencyActionContext = createContext<Pick<CurrencyContextType, 'setCurren
 });
 
 const CurrencyProvider = ({ children }: { children: React.ReactNode }) => {
+  const { data: exchangeRate } = useSuspenseQuery(getExchangeRateQueryOptions());
   const [currency, setCurrency] = useState<Currency>('USD');
   return (
-    <currencyContext.Provider value={{ currency }}>
+    <currencyContext.Provider value={{ currency, exchangeRate }}>
       <currencyActionContext.Provider value={{ setCurrency }}>{children}</currencyActionContext.Provider>
     </currencyContext.Provider>
   );
--- moon-cheese-2/src/remotes/exchange.ts
+++ moon-cheese-2/src/remotes/exchange.ts
@@ -1,13 +1,14 @@
 import { http } from '@/utils/http';
 
+export interface ExchangeRate {
+  KRW: number;
+  USD: number;
+}
 interface ExchangeRateResponse {
-  exchangeRate: {
-    KRW: number;
-    USD: number;
-  };
+  exchangeRate: ExchangeRate;
 }
 
-export const getExchangeRate = async () => {
+export const getExchangeRate = async (): Promise<ExchangeRate> => {
   const response = await http.get<ExchangeRateResponse>('/api/exchange-rate');
-  return response;
+  return response.exchangeRate;
 };

91a7c13cdc4f3bb710d0a19d5d653a380befd33d
Youna Joo <you.na.joo0@gmail.com>
2025-12-08 21:56:49 +0900

    feat: CartProvider, CurrencyProvider 래핑

--- moon-cheese-2/src/providers/GlobalProvider.tsx
+++ moon-cheese-2/src/providers/GlobalProvider.tsx
@@ -1,10 +1,16 @@
 import { EnhancedToastProvider } from '@/ui-lib/components/toast';
 import { QueryProvider } from './query-provider';
+import { CurrencyProvider } from './currency-provider';
+import { CartProvider } from './cart-provider';
 
 const GlobalProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <QueryProvider>
-      <EnhancedToastProvider>{children}</EnhancedToastProvider>
+      <CurrencyProvider>
+        <CartProvider>
+          <EnhancedToastProvider>{children}</EnhancedToastProvider>
+        </CartProvider>
+      </CurrencyProvider>
     </QueryProvider>
   );
 };
